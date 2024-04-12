d5971e2f37759c3270ca9a23dc3ef2784101a440
Leonardo Hansa <hansa.leonardo@gmail.com>
2024-04-12 23:22:00 +0200

    mejora app1

--- app1/server.R
+++ app1/server.R
@@ -1,15 +1,38 @@
 library(shiny)
+library(readr)
 library(dplyr)
 library(ggplot2)
+library(openai)
+library(plotly)
 
 # Define server logic
 server <- function(input, output) {
   
   # Suponiendo que 'df' es tu conjunto de datos ya cargado
-  df <- readr::read_csv("../data/credit_card_fraud.csv")
+  # df <- readr::read_csv("data/credit_card_fraud.csv")
+  df <- read_csv(
+    file = "../data/credit_card_fraud.csv", 
+    col_types = cols(
+      trans_date_trans_time = col_datetime(format = ""),
+      merchant = col_character(),
+      category = col_character(),
+      amt = col_double(),
+      city = col_character(),
+      state = col_character(),
+      lat = col_double(),
+      long = col_double(),
+      city_pop = col_double(),
+      job = col_character(),
+      dob = col_date(format = ""),
+      trans_num = col_character(),
+      merch_lat = col_double(),
+      merch_long = col_double(),
+      is_fraud = col_double()
+    )
+  )
   
   # Exploración 1: Distribución de transacciones por categoría
-  output$plotCategory <- renderPlot({
+  output$plot_category <- renderPlot({
     df |>
       count(category, sort = TRUE) |>
       ggplot(aes(x = reorder(category, n), y = n)) +
@@ -18,33 +41,44 @@ server <- function(input, output) {
       labs(x = "Categoría", y = "Número de Transacciones", title = "Distribución por Categoría")
   })
   
-  # Exploración 2: Monto de las transacciones por estado
-  output$plotStateAmount <- renderPlot({
+  # Exploración 3: Relación entre el monto de la transacción y la población de la ciudad
+  output$plot_state_count <- renderTable({
     df |>
+      count(state, category) |> 
+      tidyr::pivot_wider(names_from = state, values_from = n)
+  })
+  
+  # Exploración 2: Monto de las transacciones por estado
+  output$plot_state <- plotly::renderPlotly({
+    P <- df |>
       group_by(state) |>
       summarise(TotalAmount = sum(amt)) |>
       ggplot(aes(x = reorder(state, TotalAmount), y = TotalAmount)) +
       geom_bar(stat = "identity") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
       labs(x = "Estado", y = "Monto Total", title = "Monto de Transacciones por Estado")
+    
+    ggplotly(P)
   })
   
-  # Exploración 3: Relación entre el monto de la transacción y la población de la ciudad
-  output$plotAmountVsPopulation <- renderPlot({
-    df |>
-      ggplot(aes(x = city_pop, y = amt)) +
-      geom_point(alpha = 0.5) +
-      scale_x_log10() +
-      labs(x = "Población de la Ciudad (log)", y = "Monto de la Transacción", title = "Monto vs. Población de la Ciudad")
-  })
   
   # Exploración 4: Distribución del fraude vs. transacciones legítimas
-  output$plotFraudDistribution <- renderPlot({
+  output$plot_fraud_evolution <- renderPlot({
     df |>
-      ggplot(aes(x = as.factor(is_fraud), fill = as.factor(is_fraud))) +
-      geom_bar() +
-      scale_fill_manual(values = c("0" = "blue", "1" = "red")) +
-      labs(x = "Es Fraude", y = "Número de Transacciones", title = "Distribución de Fraude vs. Legítimas", fill = "Tipo")
+      mutate(date = as.Date(trans_date_trans_time), 
+             date = lubridate::floor_date(date, "month")) |> 
+      group_by(date) |>
+      summarise(frauds = n(), .groups = "drop") |> 
+      ggTimeSeries::ggplot_waterfall("date", "frauds")
+    
+  })
+  
+  output$text_chatgpt <- renderText({
+    "Los datos están bien."
+    # create_completion(
+    #   model = "gpt-3.5-turbo-instruct",
+    #   prompt = "Generate a question and an answer"
+    # )
   })
 }
 
--- app1/ui.R
+++ app1/ui.R
@@ -2,14 +2,15 @@ ui <- fluidPage(
   titlePanel("Exploración de Fraude en Tarjetas de Crédito"),
   sidebarLayout(
     sidebarPanel(
-      helpText("Explora el conjunto de datos sobre fraude en tarjetas de crédito.")
+      helpText("Explora el conjunto de datos sobre fraude en tarjetas de crédito."), 
+      textOutput("text_chatgpt")
     ),
     mainPanel(
       tabsetPanel(
-        tabPanel("Categoría", plotOutput("plotCategory")),
-        tabPanel("Monto por Estado", plotOutput("plotStateAmount")),
-        tabPanel("Monto vs. Población", plotOutput("plotAmountVsPopulation")),
-        tabPanel("Fraude vs. Legítimo", plotOutput("plotFraudDistribution"))
+        tabPanel("Categoría", plotOutput("plot_category")),
+        tabPanel("Estado y Categoría", tableOutput("plot_state_count")),
+        tabPanel("Fraudes por Estado", plotly::plotlyOutput("plot_state")),
+        tabPanel("Evolución Mensual", plotOutput("plot_fraud_evolution"))
       )
     )
   )
