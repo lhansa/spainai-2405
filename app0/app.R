21ab945165b09687c75687f1dfaed2333decb416
Leonardo Hansa <hansa.leonardo@gmail.com>
2024-05-23 17:23:24 +0200

    retoques app 0

--- app0/app.R
+++ app0/app.R
@@ -16,7 +16,9 @@ ui <- fluidPage(
   titlePanel("Exploración de Fraude en Tarjetas de Crédito"),
   sidebarLayout(
     sidebarPanel(
-      helpText("Explora el conjunto de datos sobre fraude en tarjetas de crédito.")
+      helpText(
+        "Explora el conjunto de datos sobre fraude en tarjetas de crédito."
+      )
     ),
     mainPanel(
       plotOutput("plotCategory")
@@ -27,17 +29,17 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   
-  # Suponiendo que 'df' es tu conjunto de datos ya cargado
   df <- readr::read_csv("../data/credit_card_fraud.csv", show_col_types = FALSE)
   
   # Exploración 1: Distribución de transacciones por categoría
   output$plotCategory <- renderPlot({
     df |>
-      dplyr:::count(category, sort = TRUE) |>
+      dplyr::count(category, sort = TRUE) |>
       ggplot(aes(x = reorder(category, n), y = n)) +
       geom_bar(stat = "identity") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      labs(x = "Categoría", y = "Número de Transacciones", title = "Distribución por Categoría")
+      labs(x = "Categoría", y = "Número de Transacciones", 
+           title = "Distribución por Categoría")
   })
 }
 # Run the application 
